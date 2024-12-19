b0c0399157947c2403ed48b0ffc49913fc8b1a6e
Grizzlazy <134766687+Grizzlazy@users.noreply.github.com>
2024-12-19 16:13:36 +0700

    Run-100-3-ite

--- 2_0.3.py
+++ 2_0.3.py
@@ -477,7 +477,7 @@ for txt_file in txt_files:
             best_fitness, best_sol, data_to_write = Tabu_search_for_CVRP(1)
             end_time = time.time()
             data_to_write["runtime"] = end_time - start_time
-            with open('Random_'+str(data_set)+'_'+str(number_of_cities)+'_'+str(SEGMENT)+'_iter-'+str(i)+'_CL2.json', 'w') as file:  # Open a file in write mode
+            with open('Random_'+str(data_set)+'_'+str(number_of_cities)+'_'+str(SEGMENT)+'_iter-'+str(ite)+'_CL2.json', 'w') as file:  # Open a file in write mode
                 file.write(json.dumps(data_to_write) + "\n")
             print("---------- RESULT ----------")
             print(best_sol)
