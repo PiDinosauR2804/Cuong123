56e673dc22d9155db5089f2063656a2bc91ddbd4
Grizzlazy <134766687+Grizzlazy@users.noreply.github.com>
2025-03-19 16:43:14 +0700

    0.3-0.5-thta=0.5

--- Data.py
+++ Data.py
@@ -6,13 +6,13 @@ import heapq
 import time
 import itertools
 import collections
-
-number_of_trucks = 2
+import os
+number_of_trucks = 1
 number_of_cities = 0
-number_of_drones = 2
+number_of_drones = 1
 truck_speed = 0.5
 drone_speed = 1
-drone_capacity = 4
+drone_capacity = 8
 drone_limit_time = 90
 city_demand = []
 release_date = []
@@ -25,9 +25,9 @@ DIFFERENTIAL_RATE_RELEASE_TIME = 1
 A_ratio = 1
 B_ratio = 0.7
 C_ratio = 0.1
-delta = 0.6
-alpha = [0.3, 0.2, 0.1]
-theta = 2
+delta = 0.3
+alpha = [0.5, 0.2, 0.1]
+theta = 0.5
 
 
 def read_data(path):
